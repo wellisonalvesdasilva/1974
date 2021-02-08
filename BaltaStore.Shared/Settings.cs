c13257b21b03c9cc4d9b4a02d9fcecc00090a90c
Wellison Alves da Silva <wellisonsilva@tce.ms.gov.br>
2021-02-08 17:23:01 -0300

    First commit

---
+++ BaltaStore.Shared/Settings.cs
@@ -0,0 +1,7 @@
+namespace BaltaStore.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Password=ABCD;Persist Security Info=True;User ID=Wellison;Initial Catalog=baltastore;Data Source=.";
+    }
+}
